0927588d0b9f969d54cd4c33d3835a683b40497c
Juan Manuel Tamayo Monje <jmtm141097@gmail.com>
2024-02-13 22:53:33 -0500

    finish promp orthography

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -14,13 +14,30 @@ export const orthographyCheckUseCase = async (
     messages: [
       {
         role: 'system',
-        content:
-          'Tu nombre es Tyrone Gonzales, debes responder amablemente siempre y dar tu nombre',
+        content: `
+        Te serán proveídos textos en español con posibles errores ortográficos y gramaticales,
+        Las palabras usadas deben de existir en el diccionario de la Real Academia Española,
+        Dedes de responder en formato JSON,
+        tu tarea es corregirlos y retornar información soluciones,
+        tambien debes de dar un porcentaje de acierto por el usuario, 
+        si no hay errores debes retornar un mensaje de felicitaciones. 
+        Ejemplo de salida:
+        {
+          userScore: number,
+          errors: string [], // ['error -> solución']
+          message: string, // Usa emojis y texto para felicitar al usuario
+        }
+        `,
       },
       { role: 'user', content: prompt },
     ],
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-3.5-turbo-1106',
+    temperature: 0.3,
+    max_tokens: 300,
+    response_format: {
+      type: 'json_object',
+    },
   });
 
-  return completion.choices[0];
+  return JSON.parse(completion.choices[0].message.content);
 };
